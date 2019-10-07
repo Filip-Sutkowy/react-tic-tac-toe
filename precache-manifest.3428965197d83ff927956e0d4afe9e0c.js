e85d37caacf34f5ad9cfcfc4451e448e462e3e91
Filip Sutkowy <filip.sutkowy@gmail.com>
2019-10-08 01:13:40 +0200

    Updates

---
+++ precache-manifest.3428965197d83ff927956e0d4afe9e0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130d3e8d43ac0711b40fae42eb008662",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3288ef25ac8008c5e9e3",
+    "url": "/react-tic-tac-toe/static/css/main.96301b5f.chunk.css"
+  },
+  {
+    "revision": "56d54f0f4192705cabcd",
+    "url": "/react-tic-tac-toe/static/js/2.e90969e7.chunk.js"
+  },
+  {
+    "revision": "3288ef25ac8008c5e9e3",
+    "url": "/react-tic-tac-toe/static/js/main.e12f4b0d.chunk.js"
+  },
+  {
+    "revision": "1564b412110a03060233",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.93451e4a.js"
+  }
+]);
